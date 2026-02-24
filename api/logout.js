d5426613cbdef56fb405e54dfafe450924149389
jessica <jessicadaianaandre@gmail.com>
2026-02-24 13:25:58 -0300

    Creo endpoint de logout y actualizo gestionar para cerrar sesión

---
+++ api/logout.js
@@ -0,0 +1,22 @@
+const { cookieSerialize } = require("./_lib");
+
+module.exports = async (req, res) => {
+  if (req.method !== "POST" && req.method !== "GET") {
+    res.statusCode = 405;
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    return res.end(JSON.stringify({ error: "Method not allowed" }));
+  }
+
+  const expiredSessionCookie = cookieSerialize("bunker_session", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Lax",
+    path: "/",
+    maxAge: 0,
+  });
+
+  res.setHeader("Set-Cookie", expiredSessionCookie);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  return res.end(JSON.stringify({ ok: true }));
+};
